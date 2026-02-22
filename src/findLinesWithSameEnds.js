72130cf0e51e17080b3699091495e01a80e2eee2
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2026-02-22 19:36:23 +0100

    cut(): move from findLinesWithSameEnds

--- src/findLinesWithSameEnds.js
+++ src/findLinesWithSameEnds.js
@@ -1,5 +1,5 @@
 const turf = require('@turf/turf')
-const getCommonLength = require('./getCommonLength.js')
+const cut = require('./cut.js')
 
 module.exports = function findLinesWithSameEnds (a, b, result, aPrefix, bPrefix, options) {
   let restart = false
@@ -44,51 +44,4 @@ module.exports = function findLinesWithSameEnds (a, b, result, aPrefix, bPrefix,
       ai--
     }
   }
-}
-
-function cut (a, ai, aDir, aPrefix, b, bi, bDir, bPrefix, result, options) {
-  const aItem = a.features[ai]
-  const bItem = b.features[bi]
-
-  const aLength = turf.length(aItem, { units: 'meters' })
-  const bLength = turf.length(bItem, { units: 'meters' })
-  const length = getCommonLength(aItem, aDir === 1 ? 0 : aLength, aDir, bItem, bDir === 1 ? 0 : bLength, bDir, options)
-
-  if (length == 0) {
-    return false
-  }
-
-  //console.log('length', length)
-  const item = aDir === 1 ?
-    turf.lineSliceAlong(aItem, 0, length, { units: 'meters' }) :
-    turf.lineSliceAlong(aItem, aLength - length, aLength, { units: 'meters' })
-
-  Object.entries(aItem.properties).forEach(([k, v]) => {
-    item.properties[aPrefix + k] = v
-  })
-  Object.entries(bItem.properties).forEach(([k, v]) => {
-    item.properties[bPrefix + k] = v
-  })
-
-  result.features.push(item)
-
-  if (aLength > length) {
-    a.features[ai] = aDir === 1 ?
-      turf.lineSliceAlong(aItem, length, aLength, { units: 'meters' }) :
-      turf.lineSliceAlong(aItem, 0, aLength - length, { units: 'meters' })
-    a.features[ai].properties = aItem.properties
-  } else {
-    a.features[ai] = null
-  }
-
-  if (bLength > length) {
-    b.features[bi] = bDir === 1 ?
-      turf.lineSliceAlong(bItem, length, bLength, { units: 'meters' }) :
-      turf.lineSliceAlong(bItem, 0, bLength - length, { units: 'meters' })
-    b.features[bi].properties = bItem.properties
-  } else {
-    b.features[bi] = null
-  }
-
-  return true
 }
