f57cc88d996d6011244d3001428f92ee041cab3d
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2026-02-22 15:28:40 +0100

    getCommonLength(): move from findLinesWithSameEnds

--- src/findLinesWithSameEnds.js
+++ src/findLinesWithSameEnds.js
@@ -1,4 +1,5 @@
 const turf = require('@turf/turf')
+const getCommonLength = require('./getCommonLength.js')
 
 module.exports = function findLinesWithSameEnds (a, b, result, aPrefix, bPrefix, options) {
   let restart = false
@@ -106,19 +107,4 @@ function cut (a, ai, aPrefix, b, bi, bPrefix, result, options) {
   }
 
   return true
-}
-
-function getCommonLength (a, b, options) {
-  let pos = 0
-  const step = 1
-  const maxLength = Math.min(turf.length(a, { units: 'meters' }), turf.length(b, { units: 'meters' }))
-
-  for (let pos = 0; pos < maxLength; pos += step) {
-    const d = turf.distance(turf.along(a, pos, { units: 'meters' }), turf.along(b, pos, { units: 'meters' }), { units: 'meters' })
-    if (d >= options.distance) {
-      return Math.max(pos - step - options.distance, 0)
-    }
-  }
-
-  return maxLength
 }
